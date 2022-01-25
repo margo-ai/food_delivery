ab709a710a8127dc8d5627b187ac5404dd1ebc96
Margo <plokhova.margarita@yandex.ru>
2022-01-25 18:21:46 +0300

    work with sending forms to server

--- js/script.js
+++ js/script.js
@@ -96,8 +96,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	//Modal
 
 	const modalTrigger = document.querySelectorAll('[data-modal]'),
-		  modal = document.querySelector('.modal'),
-		  modalCloseBtn = document.querySelector('[data-close]');
+		  modal = document.querySelector('.modal');
 
 	function openModal() {
 		modal.classList.add('show');
@@ -117,10 +116,8 @@ window.addEventListener('DOMContentLoaded', () => {
 		document.body.style.overflow = '';
 	}
 
-	modalCloseBtn.addEventListener('click', closeModal);
-
 	modal.addEventListener('click', (e) => {
-		if (e.target === modal) {
+		if (e.target === modal || e.target.getAttribute('data-close') == '') {
 			closeModal();
 		}
 	});
@@ -131,7 +128,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-	// const modalTimerId = setTimeout(openModal, 5000);
+	const modalTimerId = setTimeout(openModal, 50000);
 
 	function showModalByScroll() {
 		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -163,6 +160,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		render() {
 			const element = document.createElement('div');
+
 			if(this.classes.length === 0) {
 				this.element = 'menu__item';
 				element.classList.add(this.element);
@@ -200,7 +198,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		'В меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
 		14,
 		'.menu .container',
-		'menu__item'		
+		'menu__item'	
 	).render();
 
 	new MenuCard(
@@ -210,7 +208,85 @@ window.addEventListener('DOMContentLoaded', () => {
 		'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
 		21,
 		'.menu .container',
-		'menu__item'		
+		'menu__item'	
 	).render();
 	
+
+	//Forms
+
+	const forms = document.querySelectorAll('form');
+
+	const message = {
+		loading: 'img/form/spinner.svg',
+		success: 'Cпасибо! Скоро мы с Вами свяжемся!',
+		failure: 'Что-то пошло не так...'
+	};
+
+	forms.forEach(item => {
+		postData(item);
+	});
+
+	function postData(form) {
+		form.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			const statusMessage = document.createElement('img');
+			statusMessage.src = message.loading;
+			statusMessage.style.cssText = `
+				display: block;
+				margin: 0 auto;
+			`;			
+			form.insertAdjacentElement('afterend', statusMessage);
+
+			const request = new XMLHttpRequest();
+			request.open('POST', 'server.php');
+
+			request.setRequestHeader('Content-type', 'application/json');
+			const formData = new FormData(form);
+
+			const object = {};
+			formData.forEach(function(value, key){
+				object[key] = value;
+			});
+
+			const json = JSON.stringify(object);
+
+			request.send(json);
+
+			request.addEventListener('load', () => {
+				if (request.status === 200) {
+					console.log(request.response);
+					showThanksModal(message.success);
+					form.reset();		
+					statusMessage.remove();					
+				} else {
+					showThanksModal(message.failure);
+				}
+			});
+		});
+	}
+
+	function showThanksModal(message) {
+		const prevModalDialog = document.querySelector('.modal__dialog');
+
+		prevModalDialog.classList.add('hide');
+		openModal();
+
+		const thanksModal = document.createElement('div');
+		thanksModal.classList.add('modal__dialog');
+		thanksModal.innerHTML = `
+		<div class="modal__content">
+			<div class="modal__close" data-close>&times;</div>
+			<div class="modal__title">${message}</div>
+		</div>
+		`;
+
+		document.querySelector('.modal').append(thanksModal);
+		setTimeout(() => {
+			thanksModal.remove();
+			prevModalDialog.classList.add('show');
+			prevModalDialog.classList.remove('hide');
+			closeModal();
+		}, 4000);
+	}
 });
