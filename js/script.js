fafc3205337b009bcf787eedf24ca080af9d0f80
Margo <plokhova.margarita@yandex.ru>
2022-01-27 01:02:31 +0300

    installed npm package

--- js/script.js
+++ js/script.js
@@ -289,5 +289,8 @@ window.addEventListener('DOMContentLoaded', () => {
 		}, 5000);
 	}
 
+	fetch('http://localhost:3000/menu')
+		.then(data => data.json())
+		.then(res => console.log(res));
 	
 });
