3280ec330eb3241b55b1df22db1521698f8bb57b
Margo <plokhova.margarita@yandex.ru>
2022-01-30 20:14:17 +0300

    ended project with adding polyfills

--- js/modules/forms.js
+++ js/modules/forms.js
@@ -36,7 +36,7 @@ function forms(formSelector, modalTimerId) {
 			
 			postData('http://localhost:3000/requests', json)		
 			.then(data => {
-				console.log(data);
+				// console.log(data);
 				showThanksModal(message.success);					
 				statusMessage.remove();
 			}).catch(() => {
