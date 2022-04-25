50a158d91218d913094aca94963baa99970ef805
Margo <plokhova.margarita@yandex.ru>
2022-04-25 13:42:10 +0300

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
 	modal('[data-modal]', '.modal', modalTimerId);
-	timer('.timer', '2022-03-01');
+	timer('.timer', '2022-06-01');
 	cards();
 	calc();
 	forms('form', modalTimerId);
